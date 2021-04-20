240c565ded114db843a25c28602826da47dd4aa0
Jinen Setpal <jinens8@gmail.com>
2021-04-21 02:46:23 +0530

    another experimental backup push

--- app/src/main/java/com/necter/vision/MainActivity.java
+++ app/src/main/java/com/necter/vision/MainActivity.java
@@ -1,6 +1,7 @@
 package com.necter.vision;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -9,8 +10,12 @@ import android.graphics.Color;
 import android.graphics.ImageFormat;
 import android.graphics.Rect;
 import android.graphics.YuvImage;
-import android.media.Image;
 import android.os.Bundle;
+import android.renderscript.Allocation;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicYuvToRGB;
+import android.renderscript.Type;
 import android.util.Log;
 import android.util.Size;
 import android.widget.ImageView;
@@ -28,7 +33,6 @@ import androidx.core.content.ContextCompat;
 import androidx.core.util.Pair;
 
 import com.google.common.util.concurrent.ListenableFuture;
-import com.quickbirdstudios.yuv2mat.Yuv;
 
 import org.opencv.android.Utils;
 import org.opencv.core.CvType;
@@ -41,7 +45,6 @@ import org.tensorflow.lite.task.vision.segmenter.Segmentation;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.List;
@@ -85,7 +88,6 @@ public class MainActivity extends AppCompatActivity {
         cameraProviderFuture.addListener(() -> {
             try {
                 ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
-//                bindPreview(cameraProvider);
                 getInference(cameraProvider);
             } catch (ExecutionException | InterruptedException ignored) {
             }
@@ -130,7 +132,6 @@ public class MainActivity extends AppCompatActivity {
     void runInference(Bitmap bitmap) {
         try {
             TensorImage image = TensorImage.fromBitmap(bitmap);
-
             List<Segmentation> results = imageSegmenter.segment(image);
             Pair<Bitmap, Map<String, Integer>> result = createMaskBitmapAndLabels(results.get(0), image.getWidth(), image.getHeight());
             runOnUiThread(() -> outputView.setImageBitmap(stackBitmaps(Objects.requireNonNull(result.first), image.getBitmap())));
@@ -152,26 +153,46 @@ public class MainActivity extends AppCompatActivity {
 
         ImageAnalysis imageAnalysis =
                 new ImageAnalysis.Builder()
-                        .setTargetResolution(new Size(257, 257))
+                        .setTargetResolution(new Size(1280, 720))
                         .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
                         .build();
 
-        imageAnalysis.setAnalyzer(Executors.newSingleThreadExecutor(), image -> runInference(imageProxyToBitmap(image)));
+        imageAnalysis.setAnalyzer(Executors.newSingleThreadExecutor(), image -> {
+            runInference(imageProxyToBitmap(image));
+            image.close();
+        });
 
         cameraProvider.bindToLifecycle(this, cameraSelector, imageAnalysis, preview);
     }
 
-    private Bitmap imageProxyToBitmap(ImageProxy image) {
-        Log.d("INFERENCE", String.valueOf(image.getFormat()));
-        ByteBuffer byteBuffer = image.getPlanes()[0].getBuffer();
-        byteBuffer.rewind();
-        byte[] bytes = new byte[byteBuffer.capacity()];
-        byteBuffer.get(bytes);
-        byte[] clonedBytes = bytes.clone();
-        return BitmapFactory.decodeByteArray(clonedBytes, 0, clonedBytes.length);
+//    @Override
+//    public void onPreviewFrame(byte[] data, Camera camera) {
+//        Bitmap bitmap = Bitmap.createBitmap(camera.getParameters().getPreviewSize().width(), r.height(), Bitmap.Config.ARGB_8888);
+//        Allocation bmData = renderScriptNV21ToRGBA8888(
+//                this, r.width(), r.height(), data);
+//        bmData.copyTo(bitmap);
+//    }
+
+    public Allocation renderScriptNV21ToRGBA8888(Context context, int width, int height, byte[] nv21) {
+        RenderScript rs = RenderScript.create(context);
+        ScriptIntrinsicYuvToRGB yuvToRgbIntrinsic = ScriptIntrinsicYuvToRGB.create(rs, Element.U8_4(rs));
+
+        Type.Builder yuvType = new Type.Builder(rs, Element.U8(rs)).setX(nv21.length);
+        Allocation in = Allocation.createTyped(rs, yuvType.create(), Allocation.USAGE_SCRIPT);
+
+        Type.Builder rgbaType = new Type.Builder(rs, Element.RGBA_8888(rs)).setX(width).setY(height);
+        Allocation out = Allocation.createTyped(rs, rgbaType.create(), Allocation.USAGE_SCRIPT);
+
+        in.copyFrom(nv21);
+
+        yuvToRgbIntrinsic.setInput(in);
+        yuvToRgbIntrinsic.forEach(out);
+        return out;
     }
 
-    private Bitmap convertImageProxyToBitmap(ImageProxy image) {
+    private Bitmap imageProxyToBitmap(ImageProxy image) {
+        Log.d("INFERENCE", String.valueOf(image.getFormat()));
+
         ImageProxy.PlaneProxy[] planes = image.getPlanes();
         ByteBuffer yBuffer = planes[0].getBuffer();
         ByteBuffer uvBuffer = planes[2].getBuffer();
@@ -187,50 +208,27 @@ public class MainActivity extends AppCompatActivity {
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         yuv.compressToJpeg(new Rect(0, 0, yuv.getWidth(), yuv.getWidth()), 50, out);
         byte[] imageBytes = out.toByteArray();
-        return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
-    }
+//        return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
 
-    private Bitmap conversion(ImageProxy image) {
-        Mat mat = Yuv.rgb(image.getImage());
+        ByteBuffer buffer = image.getPlanes()[0].getBuffer();
+        byte[] bytes = new byte[buffer.remaining()];
+        buffer.get(bytes);
 
-        ImageProxy.PlaneProxy[] planes = image.getPlanes();
-        ByteBuffer yBuffer = planes[0].getBuffer();
-        ByteBuffer uBuffer = planes[1].getBuffer();
-        ByteBuffer vBuffer = planes[2].getBuffer();
+        Bitmap b = Bitmap.createBitmap(image.getHeight(), image.getWidth(), Bitmap.Config.ARGB_8888);
 
-        yBuffer.position(0);
-        uBuffer.position(0);
-        vBuffer.position(0);
+        Mat mat = new Mat(image.getHeight()+image.getHeight()/2, image.getWidth(), CvType.CV_8UC1);
+        mat.put(0, 0, bytes);
+        Mat rgb = new Mat(image.getHeight(), image.getWidth(), CvType.CV_8UC4);
+        Imgproc.cvtColor(mat, rgb, Imgproc.COLOR_YUV420sp2BGR, 4);
+        Utils.matToBitmap(rgb, b);
 
-        int ySize = yBuffer.remaining();
-        int uSize = uBuffer.remaining();
-        int vSize = vBuffer.remaining();
+        return b;
 
-        ByteBuffer buffer = ByteBuffer.allocateDirect(ySize + uSize + vSize);
-        buffer.put(yBuffer);
-        buffer.put(uBuffer);
-        buffer.put(vBuffer);
-
-        Mat yuvMat = new Mat(257, 257, CvType.CV_8UC1);
-        yuvMat.put(0, 0, buffer.array());
-
-        Mat rgbMat = new Mat(yuvMat.rows(), yuvMat.cols(), CvType.CV_8UC4);
-        Imgproc.cvtColor(yuvMat, rgbMat, Imgproc.COLOR_YUV420sp2RGBA);
-
-        final Bitmap bit = Bitmap.createBitmap(rgbMat.cols(), rgbMat.rows(), Bitmap.Config.ARGB_8888);
-        Utils.matToBitmap(rgbMat, bit);
-
-        byte[] nv21 = new byte[ySize + uSize + vSize];
-
-        yBuffer.get(nv21, 0, ySize);
-        vBuffer.get(nv21, ySize, vSize);
-        uBuffer.get(nv21, ySize + vSize, uSize);
-
-        YuvImage yuvImage = new YuvImage(nv21, ImageFormat.NV21, image.getWidth(), image.getHeight(), null);
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        yuvImage.compressToJpeg(new Rect(0, 0, yuvImage.getWidth(), yuvImage.getHeight()), 100, out);
-        byte[] imageBytes = out.toByteArray();
-
-        return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+//        ByteBuffer byteBuffer = image.getPlanes()[0].getBuffer();
+//        byteBuffer.rewind();
+//        byte[] bytes = new byte[byteBuffer.capacity()];
+//        byteBuffer.get(bytes);
+//        byte[] clonedBytes = bytes.clone();
+//        return BitmapFactory.decodeByteArray(clonedBytes, 0, clonedBytes.length);
     }
 }
